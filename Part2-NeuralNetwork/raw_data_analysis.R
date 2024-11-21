7c8cab2ccd9eb31eecae02035dfa487386a67fdf
Sascha Frossard <sascha.frossard@students.fhnw.ch>
2024-11-21 19:22:27 +0100

    Finalize first analysis

--- Part2-NeuralNetwork/raw_data_analysis.R
+++ Part2-NeuralNetwork/raw_data_analysis.R
@@ -85,28 +85,28 @@ columns_to_remove <- c("ID")
 
 data <- data[ , !(names(data) %in% columns_to_remove)]
 
-# CODE_GENDER
+# CODE_GENDER - Gender
 # data seems fine. No missing values and all data is binary. Therefore onehot encoding.
 str(data$CODE_GENDER)
 summary(data$CODE_GENDER)
 
 data <- clean_column(data, "CODE_GENDER", onehot_encode = TRUE)
 
-# FLAG_OWN_CAR
+# FLAG_OWN_CAR - Is there a car
 # data seems fine. No missing values and all data is binary. Therefore onehot encoding.
 str(data$FLAG_OWN_CAR)
 summary(data$FLAG_OWN_CAR)
 
 data <- clean_column(data, "FLAG_OWN_CAR", onehot_encode = TRUE)
 
-# FLAG_OWN_REALTY
+# FLAG_OWN_REALTY - Is there a property
 # data seems fine. No missing values and all data is binary. Therefore onehot encoding.
 str(data$FLAG_OWN_REALTY)
 summary(data$FLAG_OWN_REALTY)
 
 data <- clean_column(data, "FLAG_OWN_REALTY", onehot_encode = TRUE)
 
-# CNT_CHILDREN
+# CNT_CHILDREN - Number of children
 # Ordered values and Numeric. Third quantile is 1 so we do not use our function to remove outlieres here. We do it manually.
 str(data$CNT_CHILDREN)
 summary(data$CNT_CHILDREN)
@@ -129,278 +129,119 @@ data <- data %>% filter(CNT_CHILDREN < 12)
 # We use our cleaning function only for normalization 
 data <- clean_column(data, "CNT_CHILDREN")
 
-# AMT_INCOME_TOTAL
+# AMT_INCOME_TOTAL - Annual income
 str(data$AMT_INCOME_TOTAL)
 summary(data$AMT_INCOME_TOTAL)
 # we have ouliers here but in praxis it is possible to have an anual income of over 6750000. We do not drop them
 
 data <- clean_column(data, "AMT_INCOME_TOTAL")
 
-# NAME_INCOME_TYPE
+# NAME_INCOME_TYPE - Income category 
 str(data$NAME_INCOME_TYPE)
 summary(data$NAME_INCOME_TYPE)
 
 # data seems fine. No missing values and all data is categorical (nominal). Therefore onehot encoding.
 data <- clean_column(data, "NAME_INCOME_TYPE", onehot_encode = TRUE)
 
-# NAME_EDUCATION_TYPE
+# NAME_EDUCATION_TYPE - Education level 
 str(data$NAME_EDUCATION_TYPE)
 summary(data$NAME_EDUCATION_TYPE)
 table(data$NAME_EDUCATION_TYPE)
 levels <- c("Lower secondary", "Secondary / secondary special", "Incomplete higher", "Higher education", "Academic degree")
-# data seems fine. No missing values and all data is categorical (nominal). Therefore onehot encoding.
+# data seems fine. No missing values and all data is categorical (nominal). Therefore using ordered factor.
 data <- clean_column(data, "NAME_EDUCATION_TYPE", is_ordered_factor = TRUE, order_levels = levels)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ------------------------------------------------------------------------------
-# Conversion of labels to numeric values
-
-
-# term: The number of payments on the loan.
-# "36 months", "60 months" -> levels
-unique(data$term)
-
-data$term <- as.integer(ordered(str_trim(data$term), levels=c("36 months", "60 months")))
-
-# emp_length: Employment length in years. Possible values are between 0 and 10 where 0 means less than one year and 10 means ten or more years.
-# Convert to ordered levels because of duration context.
-unique(data$emp_length)
-
-data$emp_length <- as.integer(ordered(str_trim(data$emp_length), levels=c("< 1 year", 
-                                                                          "1 year", 
-                                                                          "2 years", 
-                                                                          "3 years", 
-                                                                          "4 years", 
-                                                                          "5 years",
-                                                                          "6 years",
-                                                                          "7 years",
-                                                                          "8 years",
-                                                                          "9 years",
-                                                                          "10+ years")))
-
-
-# home_ownership: The home ownership status provided by the borrower during registration.
-# Convert one-hot because type of home ownership does not seem to be ordered, especially with ANY and OTHER.
-unique(data$home_ownership)
-
-data <- dummy_cols(data, select_columns="home_ownership", remove_selected_columns = TRUE)
-
-# verification_status: "Not Verified", "Verified", "Source Verified"
-# Ordered because of trustworthiness of information increases.
-unique(data$verification_status)
-
-data$verification_status <- as.integer(ordered(str_trim(data$verification_status), levels=c("Not Verified", "Verified", "Source Verified")))
-
-
-# verification_status_joint
-# Ordered because of trustworthiness of information increases.
-unique(data$verification_status_joint)
-  
-data$verification_status_joint <- as.integer(ordered(str_trim(data$verification_status_joint), levels=c("Not Verified", "Verified", "Source Verified")))
-
-
-# pymnt_plan: Indicates if a payment plan has been put in place for the loan. Values: "y", "n"
-# Apply order n -> y because it might be better to have a plan than not.
-unique(data$pymnt_plan)
-
-data$pymnt_plan <- as.integer(ordered(str_trim(data$pymnt_plan), levels=c("n", "y")))
-
-
-# purpose: A category provided by the borrower for the loan request.
-# Apply one-hot because no order makes sense for purpose.
-
-unique(data$purpose)
-
-data <- dummy_cols(data, select_columns="purpose", remove_selected_columns = TRUE)
-
-
-# application_type: Indicates whether the loan is an individual application or a joint application with two co-borrowers. INDIVIUDAL, JOINT
-# Apply ordered probably joint is lower risk than individual
-unique(data$application_type)
-
-data$application_type <- as.integer(ordered(str_trim(data$application_type), levels=c("INDIVIUDAL", "JOINT")))
-
-# initial_list_status: The initial listing status of the loan. Possible values are – W, F -> No idea what W, F means.
-summary(data$initial_list_status)
-unique(data$initial_list_status)
-
-data$initial_list_status <- as.integer(ordered(str_trim(data$initial_list_status), levels=c("f", "w")))
-
-
-# ------------------------------------------------------------------------------
-# Numeric
-
-# loan_amnt: The listed amount of the loan applied for by the borrower. If at some point in time, the credit department reduces the loan amount, then it will be reflected in this value.
-summary(data$loan_amnt)
-
-# funded_amnt: The total amount committed to that loan at that point in time.
-summary(data$funded_amnt)
-
-# funded_amnt_inv: The total amount committed by investors for that loan at that point in time.
-summary(data$funded_amnt_inv)
-
-# installment: The monthly payment owed by the borrower if the loan originates.
-summary(data$installment)
-
-# annual_inc: The self-reported annual income provided by the borrower during registration.
-summary(data$annual_inc)
-
-# dti: A ratio calculated using the borrower’s total monthly debt payments on the total debt obligations, excluding mortgage and the requested LC loan, divided by the borrower’s self-reported monthly income.
-summary(data$dti)
-
-# delinq_2yrs: The number of 30+ days past-due incidences of delinquency in the borrower's credit file for the past 2 years.
-summary(data$delinq_2yrs)
-
-# inq_last_6mths: number - The number of inquiries in past 6 months (excluding auto and mortgage inquiries).
-summary(data$inq_last_6mths)
-
-# mths_since_last_delinq: The number of 30+ days past-due incidences of delinquency in the borrower's credit file for the past 2 years. Delinquency: https://www.investopedia.com/terms/d/delinquent.asp
-summary(data$mths_since_last_delinq)
-
-# mths_since_last_record:	The number of months since the last public record. Public record: https://www.thebalancemoney.com/public-records-and-your-credit-report-960740
-summary(data$mths_since_last_record)
-
-# open_acc: The number of open credit lines in the borrower's credit file.
-summary(data$open_acc)
-
-# pub_rec: Number of derogatory public records. Derogatory public record: https://www.experian.com/blogs/ask-experian/meaning-of-derogatory-public-record/?msockid=101f7315cdd36ee71e996797cca36faf
-summary(data$pub_rec)
-
-# revol_bal: Total credit revolving balance: https://www.investopedia.com/terms/r/revolvingcredit.asp
-summary(data$revol_bal)
-
-# revol_util: Revolving line utilization rate, or the amount of credit the borrower is using relative to all available revolving credit.
-summary(data$revol_util)
-
-# total_acc: The total number of credit lines currently in the borrower's credit file.
-summary(data$total_acc)
-
-# out_prncp: Remaining outstanding principal for total amount funded. https://www.investopedia.com/terms/p/principal.asp
-summary(data$out_prncp)
-
-# out_prncp_inv: Remaining outstanding principal for portion of total amount funded by investors.
-summary(data$out_prncp_inv)
-
-# total_pymnt: Payments received to date for total amount funded.
-summary(data$total_pymnt)
-
-# total_pymnt_inv:	Payments received to date for portion of total amount funded by investors.
-summary(data$total_pymnt_inv)
-
-# total_rec_prncp: Principal received to date.
-summary(data$total_rec_prncp)
-
-# total_rec_int: Interest received to date.
-summary(data$total_rec_int)
-
-# total_rec_late_fee: Late fees received to date.
-summary(data$total_rec_late_fee)
-
-
-
-# last_pymnt_amnt: Last total payment amount received.
-summary(data$last_pymnt_amnt)
-
-# collections_12_mths_ex_med: Number of collections in 12 months excluding medical collections.
-summary(data$collections_12_mths_ex_med)
-
-# mths_since_last_major_derog: Months since most recent 90-day or worse rating
-summary(data$mths_since_last_major_derog)
-
-# policy_code: publicly available policy_code=1, new products not publicly available policy_code=2
-summary(data$policy_code)
-
-# annual_inc_joint: The combined self-reported annual income provided by the co-borrowers during registration.
-summary(data$annual_inc_joint)
-
-# dti_joint: A ratio calculated using the co-borrowers' total monthly payments on the total debt obligations, excluding mortgages and the requested LC loan, divided by the co-borrowers' combined self-reported monthly income.
-summary(data$dti_joint)
-
-# verification_status_joint: Indicates if the co-borrowers' joint income was verified by LC, not verified, or if the income source was verified.
-summary(data$verification_status_joint)
-
-# acc_now_delinq: The number of accounts on which the borrower is now delinquent.
-summary(data$acc_now_delinq)
-
-# tot_coll_amt: Total collection amounts ever owed.
-summary(data$tot_coll_amt)
-
-# tot_cur_bal: Total current balance of all accounts.
-summary(data$tot_cur_bal)
-
-# open_acc_6m: Number of open trades in last 6 months.
-summary(data$open_acc_6m)
-
-# open_il_6m: Number of currently active installment trades.
-summary(data$open_il_6m)
-
-# open_il_12m: Number of installment accounts opened in past 12 months
-summary(data$open_il_12m)
-
-# open_il_24m: Number of installment accounts opened in past 24 months
-summary(data$open_il_24m)
-
-# mths_since_rcnt_il: Months since most recent installment accounts opened
-summary(data$mths_since_rcnt_il)
-
-# total_bal_il: Total current balance of all installment accounts
-summary(data$total_bal_il)
-
-# il_util: Ratio of total current balance to high credit/credit limit on all install acct
-summary(data$il_util)
-
-# open_rv_12m: Number of revolving trades opened in past 12 months.
-summary(data$open_rv_12m)
-
-# open_rv_24m: Number of revolving trades opened in past 24 months.
-summary(data$open_rv_24m)
-
-# max_bal_bc: Maximum current balance owed on all revolving accounts.
-summary(data$max_bal_bc)
-
-# all_util: Balance to credit limit on all trades.
-summary(data$all_util)
-
-# total_rev_hi_lim: Total revolving high credit/credit limit.
-summary(data$total_rev_hi_lim)
-
-# inq_fi: Number of personal finance inquiries.
-summary(data$inq_fi)
-
-# total_cu_tl: Number of finance trades.
-summary(data$total_cu_tl)
-
-# inq_last_12m: Number of credit inquiries in past 12 months.
-summary(data$inq_last_12m)
-
-
-
-# Print out to csv, easier to look at in Excel
-
-corr = cor(data)
-
-write.csv(corr, 'raw_data_corr.csv')
-
-
-
-# Test cleaned data on linear regression
-
-lm.fit1 <- lm(int_rate~., data=data)
-
-summary(lm.fit1)
+# NAME_FAMILY_STATUS - Marital status
+str(data$NAME_FAMILY_STATUS)
+summary(data$NAME_FAMILY_STATUS)
+table(data$NAME_FAMILY_STATUS)
+# data seems fine. No missing values and all data is categorical (nominal). Therefore onehot encoding.
+data <- clean_column(data, "NAME_FAMILY_STATUS", onehot_encode = TRUE)
+
+
+# NAME_HOUSING_TYPE - Way of living
+str(data$NAME_HOUSING_TYPE)
+summary(data$NAME_HOUSING_TYPE)
+table(data$NAME_HOUSING_TYPE)
+# data seems fine. No missing values and all data is categorical (nominal). Therefore onehot encoding.
+data <- clean_column(data, "NAME_HOUSING_TYPE", onehot_encode = TRUE)
+
+
+# DAYS_BIRTH
+# Count backwards from current day (0), -1 means yesterday 
+str(data$DAYS_BIRTH)
+summary(data$DAYS_BIRTH)
+hist(data$DAYS_BIRTH)
+# data seems fine. Distribution between 20 and 69 years of age. No missing values and all data is numerical.
+data <- clean_column(data, "DAYS_BIRTH")
+
+
+# DAYS_EMPLOYED
+# Count backwards from current day(0). If positive, it means the person unemployed. 
+str(data$DAYS_EMPLOYED)
+summary(data$DAYS_EMPLOYED)
+hist(data$DAYS_EMPLOYED)
+# data for unemployed needs to be adjusted. Default value is set to 365243. This creates a big gap if normalized 0-1.
+# Adjust unemployed to 1.
+data[data$DAYS_EMPLOYED > 0, "DAYS_EMPLOYED"] <- 1
+hist(data$DAYS_EMPLOYED)
+# Now distribution looks better
+data <- clean_column(data, "DAYS_EMPLOYED")
+
+
+# FLAG_MOBIL - Is there a mobile phone
+summary(data$FLAG_MOBIL)
+# All entries have value 1. No variation in data. Therefore remove column.
+data <- subset(data, select = -FLAG_MOBIL)
+
+
+# FLAG_WORK_PHONE - Is there a work phone
+summary(data$FLAG_WORK_PHONE)
+# No NAs. Data is already between 0 and 1. No changes required.
+
+
+# FLAG_PHONE - Is there a phone
+summary(data$FLAG_PHONE)
+# No NAs. Data is already between 0 and 1. No changes required.
+
+
+# FLAG_EMAIL - Is there an email
+summary(data$FLAG_EMAIL)
+# No NAs. Data is already between 0 and 1. No changes required.
+
+
+# OCCUPATION_TYPE - Occupation
+summary(as.factor(data$OCCUPATION_TYPE))
+# There are 20699 NA's which is about a third of the total rows. Create new category missing.
+data$OCCUPATION_TYPE[is.na(data$OCCUPATION_TYPE)] <- "Missing"
+data <- clean_column(data, "OCCUPATION_TYPE", onehot_encode = TRUE)
+
+
+# CNT_FAM_MEMBERS - Family size
+summary(data$CNT_FAM_MEMBERS)
+hist(data$CNT_FAM_MEMBERS)
+# Numeric value between 1 and 9, seems realistic.
+data <- clean_column(data, "CNT_FAM_MEMBERS")
+
+
+# status - “pay-back behavior” this is the classification classes
+# 0: 1-29 days past due  
+# 1: 30-59 days past due  
+# 2: 60-89 days overdue  
+# 3: 90-119 days overdue  
+# 4: 120-149 days overdue  
+# 5: Overdue or bad debts, write-offs for more than 150 days  
+# C: paid off that month  
+# X: No loan for the month 
+summary(as.factor(data$status))
+# Reorder: C is the best no overdue then 0 - 5
+# Remark: Here X was chosen even better as C but it remains to be seen if this makes sense.
+data$status <- as.integer(factor(data$status, ordered = TRUE, levels = c("X", "C", "0", "1", "2", "3", "4", "5")))
+
+
+# Plot the correlation matrix and save because of many rows it is easier to read.
+correlation_matrix <- cor(data)
+png(filename = "raw_data_corrplot.png", width = 2000, height = 2000)
+corrplot(correlation_matrix, method = "color", type = "upper", tl.col = "black", addCoef.col = "black",number.cex = 1, tl.cex = 1)
+dev.off()
